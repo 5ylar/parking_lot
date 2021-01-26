e2c213766ca457827ff89989efe8e0fd746c17b3
sylar <forzepmono@gmail.com>
2021-01-26 07:18:23 +0000

    refactor controller

--- main.go
+++ main.go
@@ -1,6 +1,10 @@
 package main
 
 import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
 	parkingController "parking_lot/parking/controller"
 	parkingRepository "parking_lot/parking/repository"
 	parkingUsecase "parking_lot/parking/usecase"
@@ -9,5 +13,48 @@ import (
 func main() {
 	parkingRepo := parkingRepository.NewParkingRepository()
 	parkingUsecase := parkingUsecase.NewParkingUsecase(parkingRepo)
-	parkingController.InitParkingController(parkingUsecase)
+	_parkingController := parkingController.NewParkingController(parkingUsecase)
+
+	args := os.Args[1:]
+	if len(args) > 0 {
+		file, err := os.Open(args[0])
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+
+		scanner := bufio.NewScanner(file)
+		for scanner.Scan() {
+			command := scanner.Text()
+			matched, err := _parkingController.ProcessCommand(command)
+			if err != nil {
+				fmt.Println(err.Error())
+			}
+			if matched {
+				continue
+			}
+			/* Can use other controllers */
+		}
+
+		if err := scanner.Err(); err != nil {
+			log.Fatal(err)
+		}
+
+	} else {
+		reader := bufio.NewReader(os.Stdin)
+		for {
+			command, err := reader.ReadString('\n')
+			if err != nil {
+				fmt.Println(err)
+			}
+			matched, err := _parkingController.ProcessCommand(command)
+			if err != nil {
+				fmt.Println(err.Error())
+			}
+			if matched {
+				continue
+			}
+			/* Can use other controllers */
+		}
+	}
 }
--- parking/controller/parking.go
+++ parking/controller/parking.go
@@ -1,10 +1,8 @@
 package controllor
 
 import (
-	"bufio"
 	"errors"
 	"fmt"
-	"log"
 	"os"
 	"parking_lot/parking"
 	"strconv"
@@ -15,44 +13,104 @@ type ParkingController struct {
 	parkingUsecase parking.Usecase
 }
 
-func InitParkingController(_parkingUsecase parking.Usecase) {
-	args := os.Args[1:]
-	if len(args) > 0 {
-		file, err := os.Open(args[0])
-		if err != nil {
-			fmt.Println(err)
-			return
-		}
-
-		scanner := bufio.NewScanner(file)
-		for scanner.Scan() {
-			cmdString := scanner.Text()
-			err := runCommand(cmdString, _parkingUsecase)
-			if err != nil {
-				fmt.Println(err.Error())
-			}
-		}
-
-		if err := scanner.Err(); err != nil {
-			log.Fatal(err)
-		}
-
-	} else {
-		reader := bufio.NewReader(os.Stdin)
-		for {
-			cmdString, err := reader.ReadString('\n')
-			if err != nil {
-				fmt.Println(err)
-			}
-			err = runCommand(cmdString, _parkingUsecase)
-			if err != nil {
-				fmt.Println(err.Error())
-			}
-		}
+func (c *ParkingController) createParkingLot(args []string) error {
+	if len(args) == 0 {
+		return errors.New("Required number of parking lot")
 	}
+	nLot, err := strconv.Atoi(args[0])
+	if err != nil {
+		return errors.New("Wrong argument for parking lot")
+	}
+
+	msg, err := c.parkingUsecase.CreateParkLot(uint16(nLot))
+	if err != nil {
+		return err
+	}
+	fmt.Println(msg)
+
+	return nil
 }
 
-func runCommand(command string, parkingUsecase parking.Usecase) error {
+func (c *ParkingController) park(args []string) error {
+	if len(args) < 2 {
+		return errors.New("Required at least 2 parameters (registeration number, colour)")
+	}
+
+	msg, err := c.parkingUsecase.Park(args[0], args[1])
+	if err != nil {
+		return err
+	}
+	fmt.Println(msg)
+
+	return nil
+}
+
+func (c *ParkingController) leave(args []string) error {
+	if len(args) == 0 {
+		return errors.New("Required number of parking lot")
+	}
+	nLot, err := strconv.Atoi(args[0])
+	if err != nil {
+		return errors.New("Wrong argument for parking lot")
+	}
+
+	msg, err := c.parkingUsecase.Leave(uint16(nLot))
+	if err != nil {
+		return err
+	}
+	fmt.Println(msg)
+	return nil
+}
+
+func (c *ParkingController) status(args []string) error {
+	msg, err := c.parkingUsecase.Status()
+	if err != nil {
+		return err
+	}
+	fmt.Println(msg)
+	return nil
+}
+
+func (c *ParkingController) findRegisNoForCarByColour(args []string) error {
+	if len(args) == 0 {
+		return errors.New("Required colour")
+	}
+
+	msg, err := c.parkingUsecase.FindRegisNoForCarByColour(args[0])
+	if err != nil {
+		return err
+	}
+	fmt.Println(msg)
+	return nil
+}
+
+func (c *ParkingController) findSlotNumberByColour(args []string) error {
+	if len(args) == 0 {
+		return errors.New("Required colour")
+	}
+
+	msg, err := c.parkingUsecase.FindSlotNumberByColour(args[0])
+	if err != nil {
+		return err
+	}
+	fmt.Println(msg)
+	return nil
+}
+
+func (c *ParkingController) findSlotNumberByRegisNo(args []string) error {
+	if len(args) == 0 {
+		return errors.New("Required registration No")
+	}
+
+	msg, err := c.parkingUsecase.FindSlotNumberByRegisNo(args[0])
+	if err != nil {
+		return err
+	}
+	fmt.Println(msg)
+	return nil
+}
+
+func (c *ParkingController) ProcessCommand(command string) (matchedCommand bool, err error) {
 	commands := strings.Fields(command)
 
 	command = commands[0]
@@ -65,89 +123,24 @@ func runCommand(command string, parkingUsecase parking.Usecase) error {
 	case "exit":
 		os.Exit(0)
 	case "create_parking_lot":
-		if len(arrAgrs) == 0 {
-			return errors.New("Required number of parking lot")
-		}
-		nLot, err := strconv.Atoi(arrAgrs[0])
-		if err != nil {
-			return errors.New("Wrong argument for parking lot")
-		}
-
-		msg, err := parkingUsecase.CreateParkLot(uint16(nLot))
-		if err != nil {
-			return err
-		}
-		fmt.Println(msg)
-
-		return nil
+		return true, c.createParkingLot(arrAgrs)
 	case "park":
-		if len(arrAgrs) < 2 {
-			return errors.New("Required at least 2 parameters (registeration number, colour)")
-		}
-
-		msg, err := parkingUsecase.Park(arrAgrs[0], arrAgrs[1])
-		if err != nil {
-			return err
-		}
-		fmt.Println(msg)
-
-		return nil
+		return true, c.park(arrAgrs)
 	case "leave":
-		if len(arrAgrs) == 0 {
-			return errors.New("Required number of parking lot")
-		}
-		nLot, err := strconv.Atoi(arrAgrs[0])
-		if err != nil {
-			return errors.New("Wrong argument for parking lot")
-		}
-
-		msg, err := parkingUsecase.Leave(uint16(nLot))
-		if err != nil {
-			return err
-		}
-		fmt.Println(msg)
-		return nil
+		return true, c.leave(arrAgrs)
 	case "status":
-		msg, err := parkingUsecase.Status()
-		if err != nil {
-			return err
-		}
-		fmt.Println(msg)
-		return nil
+		return true, c.status(arrAgrs)
 	case "registration_numbers_for_cars_with_colour":
-		if len(arrAgrs) == 0 {
-			return errors.New("Required colour")
-		}
-
-		msg, err := parkingUsecase.FindRegisNoForCarByColour(arrAgrs[0])
-		if err != nil {
-			return err
-		}
-		fmt.Println(msg)
-		return nil
+		return true, c.findRegisNoForCarByColour(arrAgrs)
 	case "slot_numbers_for_cars_with_colour":
-		if len(arrAgrs) == 0 {
-			return errors.New("Required colour")
-		}
-
-		msg, err := parkingUsecase.FindSlotNumberByColour(arrAgrs[0])
-		if err != nil {
-			return err
-		}
-		fmt.Println(msg)
-		return nil
+		return true, c.findSlotNumberByColour(arrAgrs)
 	case "slot_number_for_registration_number":
-		if len(arrAgrs) == 0 {
-			return errors.New("Required registration No")
-		}
-
-		msg, err := parkingUsecase.FindSlotNumberByRegisNo(arrAgrs[0])
-		if err != nil {
-			return err
-		}
-		fmt.Println(msg)
-		return nil
+		return true, c.findSlotNumberByRegisNo(arrAgrs)
 	}
 
-	return nil
+	return false, nil
+}
+
+func NewParkingController(_parkingUsecase parking.Usecase) *ParkingController {
+	return &ParkingController{parkingUsecase: _parkingUsecase}
 }
